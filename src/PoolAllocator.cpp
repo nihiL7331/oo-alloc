71caf845b59704c7452c95b177a3d12714b793b3
nihiL7331 <pujanekp6@gmail.com>
2026-04-27 00:21:40 +0200

    feat(core, pool): free logic

--- src/PoolAllocator.cpp
+++ src/PoolAllocator.cpp
@@ -24,12 +24,50 @@ void* PoolAllocator::alloc(std::size_t size, std::uint8_t align) {
 }
 
 void PoolAllocator::free(void* ptr) {
+  if (ptr == nullptr)
+    return;
+
+  void** ret_chunk = reinterpret_cast<void **>(ptr);
+  *ret_chunk = m_free_list_head;
+  m_free_list_head = ptr;
 }
 
 bool PoolAllocator::init(std::size_t size) {
+  if (size < m_chunk_size)
+    return false;
+  
+  if (size % m_chunk_size != 0)
+    return false;
+
+  m_start_ptr = std::malloc(size);
+  if (m_start_ptr == nullptr)
+    return false;
+
+  m_total_size = size;
+
+  clear();
+
+  return true;
 }
 
 void PoolAllocator::clear() {
+  if (m_start_ptr == nullptr)
+    return;
+
+  uint8_t* raw_mem = reinterpret_cast<uint8_t*>(m_start_ptr);
+  std::size_t num_chunks = m_total_size / m_chunk_size;
+
+  for (std::size_t i = 0; i < num_chunks - 1; ++i) {
+    void** curr_chunk = reinterpret_cast<void **>(raw_mem + (i * m_chunk_size));
+    void* next_chunk = raw_mem + ((i + 1) * m_chunk_size);
+
+    *curr_chunk = next_chunk;
+  }
+
+  void** last_chunk = reinterpret_cast<void **>(raw_mem + ((num_chunks - 1) * m_chunk_size));
+  *last_chunk = nullptr;
+
+  m_free_list_head = m_start_ptr;
 }
    
 }
