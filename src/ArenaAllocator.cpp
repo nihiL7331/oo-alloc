dd7e0da403580b9517b1e799a406b53045817e53
nihiL7331 <pujanekp6@gmail.com>
2026-04-26 21:38:16 +0200

    feat(core, arena): destructor logic

--- src/ArenaAllocator.cpp
+++ src/ArenaAllocator.cpp
@@ -1,7 +1,15 @@
 #include "ArenaAllocator.hpp"
+#include <cstdlib>
+
 namespace oo_alloc {
 
 ArenaAllocator::ArenaAllocator()
   : m_start_ptr(nullptr), m_total_size(0), m_offset(0) {}
+
+ArenaAllocator::~ArenaAllocator() {
+  if (m_start_ptr != nullptr)
+    std::free(m_start_ptr);
+}
+
    
 }
