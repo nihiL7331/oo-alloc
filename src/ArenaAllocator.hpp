744eca2344143077ca93fd08cb51c7422003004b
nihiL7331 <pujanekp6@gmail.com>
2026-04-26 20:32:27 +0200

    feat(core): arena allocator header

---
+++ src/ArenaAllocator.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "IAllocator.hpp"
+
+namespace oo_alloc {
+
+class ArenaAllocator: public IAllocator {
+private:
+  void*       m_start_ptr;
+  std::size_t m_total_size;
+  std::size_t m_offset;
+
+public:
+  ArenaAllocator();
+  ~ArenaAllocator() override;
+
+  void* alloc(std::size_t size, std::uint8_t align) override;
+  void  free(void* ptr) override;
+  bool  init(std::size_t size) override;
+
+  void clear();
+};
+
+}
