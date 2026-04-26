8c125a4b2b385a0ad216524bbd8956e313341aff
nihiL7331 <pujanekp6@gmail.com>
2026-04-27 00:43:13 +0200

    feat(tests, pool): exhaust test

---
+++ tests/pool_exhaust.cpp
@@ -0,0 +1,19 @@
+#include "PoolAllocator.hpp"
+#include <cassert>
+#include <cstdint>
+
+int main() {
+  oo_alloc::PoolAllocator pool(sizeof(uint64_t), alignof(uint64_t));
+
+  bool succ = pool.init(32);
+  assert(succ && "Failed to initialize");
+
+  void* p1 = pool.alloc(sizeof(uint64_t), alignof(uint64_t));
+  void* p2 = pool.alloc(sizeof(uint64_t), alignof(uint64_t));
+  void* p3 = pool.alloc(sizeof(uint64_t), alignof(uint64_t));
+  void* p4 = pool.alloc(sizeof(uint64_t), alignof(uint64_t));
+  assert(p1 && p2 && p3 && p4 && "Failed to allocate valid chunks");
+
+  void* p5 = pool.alloc(sizeof(uint64_t), alignof(uint64_t));
+  assert(p5 == nullptr && "Did not return nullptr when exhausted");
+}
