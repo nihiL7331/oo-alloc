acd88cc0b09e4304aff71282fcd00c560d74e7fd
nihiL7331 <pujanekp6@gmail.com>
2026-04-26 20:10:42 +0200

    feat(core): allocator interface

---
+++ src/IAllocator.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstddef>
+#include <cstdint>
+
+namespace oo_alloc {
+
+class IAllocator {
+public:
+  virtual ~IAllocator() = default;
+
+  virtual void* alloc(std::size_t size, std::uint8_t align) = 0;
+  virtual void free(void* ptr) = 0;
+  virtual bool init(std::size_t size) = 0;
+};
+
+}
