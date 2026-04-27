95041f9134d70526aa3a7af56cb1dcee5ff94337
nihiL7331 <pujanekp6@gmail.com>
2026-04-28 00:04:30 +0200

    feat(core, free): header definition

---
+++ include/oo_alloc/FreeListAllocator.hpp
@@ -0,0 +1,30 @@
+#include "oo_alloc/IAllocator.hpp"
+#include <cstdint>
+
+namespace oo_alloc {
+
+class FreeListAllocator: public IAllocator {
+private:
+  struct FreeBlock {
+    std::size_t size;
+    FreeBlock *next;
+  };
+  struct AllocHeader {
+    std::size_t size;
+    std::uint8_t pad;
+  };
+  void*       m_start_ptr;
+  std::size_t m_total_size;
+  FreeBlock* m_free_list_head;
+
+public:
+  FreeListAllocator();
+  ~FreeListAllocator() override;
+
+  void* alloc(std::size_t size, std::uint8_t align) override;
+  void  free(void* ptr) override;
+  bool  init(std::size_t size) override;
+  void  clear() override;
+};
+
+}
