1b0780187a184ce67f7237ddf1db7330bb2c2f37
nihiL7331 <pujanekp6@gmail.com>
2026-04-27 14:08:42 +0200

    feat(tests, stack): clear test

---
+++ tests/stack_clear.cpp
@@ -0,0 +1,18 @@
+#include "StackAllocator.hpp"
+#include <cassert>
+
+int main() {
+  oo_alloc::StackAllocator stack;
+
+  bool succ = stack.init(1024);
+  assert(succ && "Failed to initialize");
+
+  void* ptr = stack.alloc(32, 16);
+  stack.alloc(12, 4);
+  stack.alloc(64, 8);
+
+  stack.clear();
+
+  void* new_ptr = stack.alloc(64, 16);
+  assert(ptr == new_ptr && "clear didn't set m_offset to 0");
+}
