1f35462cfce923d75c636855632d39de8d157c46
nihiL7331 <pujanekp6@gmail.com>
2026-04-27 14:53:01 +0200

    feat(core, track): destructor logic

---
+++ src/TrackingAllocator.cpp
@@ -0,0 +1,15 @@
+#include "TrackingAllocator.hpp"
+#include <iostream>
+
+namespace oo_alloc {
+
+TrackingAllocator::~TrackingAllocator() {
+  if (!m_active_allocs.empty())
+    std::cerr << "MEM LEAK: "
+              << m_active_allocs.size()
+              << " allocs were not freed. "
+              << "(" << m_curr_alloced_bytes << "B)"
+              << std::endl;
+}
+
+}
