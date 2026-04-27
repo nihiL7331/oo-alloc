d628aeeed8532388613ecad9496b735ad7bb87e6
nihiL7331 <pujanekp6@gmail.com>
2026-04-27 13:48:10 +0200

    feat(core, stack): alloc logic

--- src/StackAllocator.cpp
+++ src/StackAllocator.cpp
@@ -1,4 +1,5 @@
 #include "StackAllocator.hpp"
+#include <algorithm>
 #include <cstdlib>
 
 namespace oo_alloc {
@@ -9,4 +10,29 @@ StackAllocator::StackAllocator()
 StackAllocator::~StackAllocator() {
   if (m_start_ptr != nullptr)
     std::free(m_start_ptr);
+}
+
+void* StackAllocator::alloc(std::size_t size, std::uint8_t align) {
+  uintptr_t base_ptr = reinterpret_cast<uintptr_t>(m_start_ptr);
+  uintptr_t curr_ptr = base_ptr + m_offset;
+
+  std::uint8_t header_align = alignof(std::size_t);
+  std::uint8_t header_size = sizeof(std::size_t);
+
+  std::uint8_t actual_align = std::max(align, header_align);
+
+  uintptr_t unalign_ptr = curr_ptr + header_size;
+  std::uint8_t pad = (actual_align - (unalign_ptr & (actual_align - 1))) & (actual_align - 1);
+  uintptr_t data_ptr = unalign_ptr + pad;
+
+  std::size_t alloc_size = header_size + pad + size;
+  if (m_offset + alloc_size > m_total_size)
+    return nullptr;
+
+  std::size_t *header_ptr = reinterpret_cast<std::size_t *>(data_ptr - header_size);
+  *header_ptr = m_offset;
+
+  m_offset += alloc_size;
+
+  return reinterpret_cast<void *>(data_ptr);
 }
