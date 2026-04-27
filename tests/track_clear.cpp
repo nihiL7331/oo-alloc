1f0f257c4dd47ddb7ea7c00a8198f09936ba652f
nihiL7331 <pujanekp6@gmail.com>
2026-04-27 15:11:46 +0200

    feat(tests, track): clear test

---
+++ tests/track_clear.cpp
@@ -0,0 +1,21 @@
+#include "TrackingAllocator.hpp"
+#include "PoolAllocator.hpp"
+#include <cassert>
+
+int main() {
+  oo_alloc::PoolAllocator pool(32, 8);
+  oo_alloc::TrackingAllocator track(pool);
+
+  bool succ = track.init(320);
+  assert(succ && "Failed to pass init to pool");
+
+  track.alloc(32, 8);
+  track.alloc(32, 8);
+  track.alloc(32, 8);
+  track.alloc(32, 8);
+
+  track.clear();
+  assert(track.get_curr_bytes() == 0 && "Curr bytes wrong post clear()");
+  assert(track.get_active_allocs_cnt() == 0 && "Active count wrong post clear()");
+  assert(track.get_peak_bytes() == 128 && "Peak bytes wrong post clear()");
+}
