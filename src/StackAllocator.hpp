71bac9b864c085b4ca42dc47fa69aa2892801d41
nihiL7331 <pujanekp6@gmail.com>
2026-04-27 13:45:26 +0200

    feat(core, stack): header definition

---
+++ src/StackAllocator.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "IAllocator.hpp"
+
+namespace oo_alloc {
+
+class StackAllocator: public IAllocator {
+private:
+  void*       m_start_ptr;
+  std::size_t m_offset;
+  std::size_t m_total_size;
+
+public:
+  StackAllocator();
+  ~StackAllocator() override;
+
+  void* alloc(std::size_t size, std::uint8_t align) override;
+  void  free(void* ptr) override;
+  bool  init(std::size_t size) override;
+  void clear() override;
+};
+
+}
