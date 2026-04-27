f6290cfc3b25ee8c8efdb2e953d235fac01ada74
nihiL7331 <pujanekp6@gmail.com>
2026-04-28 00:07:18 +0200

    feat(core, free): constructor & destructor

--- src/FreeListAllocator.cpp
+++ src/FreeListAllocator.cpp
@@ -1,5 +1,13 @@
 #include "oo_alloc/FreeListAllocator.hpp"
+#include <cstdlib>
 
 namespace oo_alloc {
 
+FreeListAllocator::FreeListAllocator()
+  : m_start_ptr(nullptr), m_total_size(0), m_free_list_head(nullptr) {}
+
+FreeListAllocator::~FreeListAllocator() {
+  if (m_start_ptr != nullptr)
+    std::free(m_start_ptr);
 }
+
