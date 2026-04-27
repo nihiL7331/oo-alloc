9ab4594a8ac8d06417a9a42a28437bb8d8432e98
nihiL7331 <pujanekp6@gmail.com>
2026-04-27 14:03:36 +0200

    feat(tests, stack): lifo test

---
+++ tests/stack_lifo.cpp
@@ -0,0 +1,17 @@
+#include "StackAllocator.hpp"
+#include <cassert>
+
+int main() {
+  oo_alloc::StackAllocator stack;
+
+  bool succ = stack.init(1024);
+  assert(succ && "Failed to initialize");
+
+  void* ptr1 = stack.alloc(12, 4);
+  void* ptr2 = stack.alloc(64, 8);
+
+  stack.free(ptr2);
+
+  void* ptr3 = stack.alloc(64, 8);
+  assert(ptr2 == ptr3 && "Header read/roll back fail");
+}
