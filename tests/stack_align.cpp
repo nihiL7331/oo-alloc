1a0db1226d4dab2d577e7bb6b0009afc1a0ce150
nihiL7331 <pujanekp6@gmail.com>
2026-04-27 14:00:54 +0200

    feat(tests, stack): align test

---
+++ tests/stack_align.cpp
@@ -0,0 +1,14 @@
+#include "StackAllocator.hpp"
+#include <cassert>
+#include <cstdint>
+
+int main() {
+  oo_alloc::StackAllocator stack;
+
+  bool succ = stack.init(1024);
+  assert(succ && "Failed to initialize");
+
+  void* ptr1 = stack.alloc(32, 16);
+  assert(ptr1 != nullptr && "Failed to allocate ptr1");
+  assert(reinterpret_cast<uintptr_t>(ptr1) % 16 == 0 && "Alignment to 16B failed");
+}
