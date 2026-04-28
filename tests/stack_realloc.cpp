df26b530c6cbd51e234f8886a75cf36bf698f0cd
nihiL7331 <pujanekp6@gmail.com>
2026-04-29 00:22:41 +0200

    feat(tests, stack): realloc test

---
+++ tests/stack_realloc.cpp
@@ -0,0 +1,31 @@
+#include "oo_alloc/StackAllocator.hpp"
+#include <cassert>
+#include <cstring>
+
+int main() {
+  oo_alloc::StackAllocator stack;
+  bool succ = stack.init(1024);
+  assert(succ && "Failed to initialize");
+
+  void *ptr1 = stack.alloc(16, 8);
+  void *ptr1_grown = stack.realloc(ptr1, 16, 32, 8);
+  assert(ptr1 == ptr1_grown && "Should return the same pointer");
+
+  char *str_ptr = static_cast<char *>(stack.alloc(16, 1));
+  std::strcpy(str_ptr, "Hello, World");
+
+  void *block = stack.alloc(16, 8);
+
+  char *str_ptr_grown = static_cast<char *>(stack.realloc(str_ptr, 16, 64, 1));
+  assert(str_ptr != str_ptr_grown &&
+         "Should return a new pointer");
+  assert(std::strcmp(str_ptr_grown, "Hello, World") == 0 &&
+         "memcpy failed to retain data");
+
+  void *str_ptr_shrunk = stack.realloc(str_ptr, 16, 8, 1);
+  assert(str_ptr == str_ptr_shrunk &&
+         "Should return the original pointer");
+
+  void *overflow_ptr = stack.realloc(str_ptr_grown, 64, 99999, 8);
+  assert(overflow_ptr == nullptr && "Should fail on overflow realloc");
+}
