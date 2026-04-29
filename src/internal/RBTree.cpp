11111d379b09b812d87f52fb15622257562edd9f
nihiL7331 <pujanekp6@gmail.com>
2026-04-29 20:39:10 +0200

    feat(core, rbtree): rotate_r

--- src/internal/RBTree.cpp
+++ src/internal/RBTree.cpp
@@ -30,9 +30,31 @@ void RBTree::rotate_l(Node* node) {
   node->parent = child_r;
 }
 
+// mirrored version of rotate_l
 void RBTree::rotate_r(Node* node) {
-  (void)node;
-  assert(false && "TODO");
+  Node* child_l = node->left;
+
+  // 1. place the right child of 'child_l'
+  // in the place of 'child_l'
+  node->left = child_l->right;
+  child_l->right->parent = node;
+
+  // 2. place 'child_l' in the place of 'node'
+  child_l->parent = node->parent;
+
+  // if 'node' has no parent, 'child_l' is the new root
+  if (node->parent == &m_sentinel)
+    m_root = child_l;
+  // place child_l accordingly
+  else if (node == node->parent->right)
+    node->parent->right = child_l;
+  else
+    node->parent->left = child_l;
+
+  // 3. place 'node' as the left child of 'child_l'
+  // (move 'node' bottom-right)
+  child_l->right = node;
+  node->parent = child_l;
 }
 
 void RBTree::insert_fix(Node* node) {
