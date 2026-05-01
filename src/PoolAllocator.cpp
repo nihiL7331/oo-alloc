5c53fc31ed3e5bfc83642f658a7eb751c3d2e555
nihiL7331 <pujanekp6@gmail.com>
2026-05-01 21:27:05 +0200

    chore(core, pool): comments

--- src/PoolAllocator.cpp
+++ src/PoolAllocator.cpp
@@ -14,6 +14,11 @@ PoolAllocator::~PoolAllocator() {
   }
 }
 
+/* alloc in the pool allocator
+ * just pops and returns the head of free list.
+ * since each chunk is of the same size, we dont need
+ * any additional searches/list traversals.
+ */
 void* PoolAllocator::alloc(std::size_t size, std::size_t align) {
   if (size > m_chunk_size || align > m_chunk_align)
     return nullptr;
@@ -26,6 +31,10 @@ void* PoolAllocator::alloc(std::size_t size, std::size_t align) {
   return ret_head;
 }
 
+/* pushes freed element as the new head
+ * of the free list, stores it in the 
+ * free data chunk itself (intrusive approach)
+ */
 void PoolAllocator::free(void* ptr) {
   if (ptr == nullptr)
     return;
@@ -49,11 +58,20 @@ bool PoolAllocator::init(std::size_t size) {
   return true;
 }
 
+/* iterates over all allocated chunks,
+ * and links them by storing the pointer
+ * to the next chunk inside of the data block,
+ * at the end setting the start pointer as
+ * the head of the free list
+ */
 void PoolAllocator::clear() {
   if (m_start_ptr == nullptr)
     return;
 
   std::uint8_t* raw_mem_ptr = reinterpret_cast<std::uint8_t*>(m_start_ptr);
+
+  // uses integer division, if m_total_size % m_chunk_size != 0,
+  // then it just leaves some trailing bytes at the end of the memory
   std::size_t num_chunks = m_total_size / m_chunk_size;
 
   for (std::size_t i = 0; i < num_chunks - 1; ++i) {
@@ -69,10 +87,13 @@ void PoolAllocator::clear() {
   m_free_list_head = m_start_ptr;
 }
    
+/* essentially a noop,
+ * reallocation is impossible due to fixed chunk size
+ */
 void *PoolAllocator::realloc(void *ptr, std::size_t old_size,
                               std::size_t new_size, std::size_t align) {
-  (void)old_size;
-  (void)align;
+  (void)old_size; (void)align;
+
   if (new_size > m_chunk_size)
     return nullptr;
   else
