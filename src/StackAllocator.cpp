8b17c33aef916bfac8b7bf08971d05474c31e4f8
nihiL7331 <pujanekp6@gmail.com>
2026-05-01 21:39:01 +0200

    chore(core, stack): comments & cleanup

--- src/StackAllocator.cpp
+++ src/StackAllocator.cpp
@@ -17,12 +17,20 @@ StackAllocator::~StackAllocator() {
   }
 }
 
+/* memory structure:
+ * [ pad ] [ header ] [ data ]
+ *
+ * calculates alignment padding,
+ * hides the header behind the data.
+ * header stores the offset of the stack BEFORE
+ * this allocation, allowing for free capabilities
+ */
 void* StackAllocator::alloc(std::size_t size, std::size_t align) {
   std::uintptr_t base_ptr = reinterpret_cast<std::uintptr_t>(m_start_ptr);
   std::uintptr_t curr_ptr = base_ptr + m_offset;
 
-  std::size_t header_align = alignof(std::size_t);
-  std::size_t header_size = sizeof(std::size_t);
+  constexpr std::size_t header_align = alignof(std::size_t);
+  constexpr std::size_t header_size = sizeof(std::size_t);
 
   std::size_t actual_align = std::max(align, header_align);
 
@@ -34,14 +42,20 @@ void* StackAllocator::alloc(std::size_t size, std::size_t align) {
   if (m_offset + alloc_size > m_total_size)
     return nullptr;
 
+  // store the offset in header
   std::size_t *header_ptr = reinterpret_cast<std::size_t *>(data_ptr - header_size);
   *header_ptr = m_offset;
 
+  // push the offset AFTER storing it in the header
   m_offset += alloc_size;
 
   return reinterpret_cast<void *>(data_ptr);
 }
 
+/* rewinds the offset, reading it from the header.
+ * because of LIFO it deallocates other data that was
+ * allocated after freed data.
+ */ 
 void StackAllocator::free(void* ptr) {
   if (ptr == nullptr)
     return;
@@ -61,19 +75,28 @@ bool StackAllocator::init(std::size_t size) {
   return true;
 }
 
+/* exactly like in the arena allocator,
+ * just move the offset back to 0
+ */
 void StackAllocator::clear() {
   m_offset = 0;
 }
 
+/* reallocation only occurs in place,
+ * if the reallocated chunk is last in.
+ * otherwise, just allocates new memory
+ * and copies the data over. 
+ * can't free older blocks, so it becomes dead space. 
+ */
 void *StackAllocator::realloc(void *ptr, std::size_t old_size,
                               std::size_t new_size, std::size_t align) {
   if (new_size <= old_size)
     return ptr;
 
-  std::uint8_t *raw_mem_ptr = reinterpret_cast<std::uint8_t *>(ptr);
-  std::uint8_t *raw_start_ptr = reinterpret_cast<std::uint8_t *>(m_start_ptr);
+  std::uint8_t* raw_mem_ptr = static_cast<std::uint8_t *>(ptr);
+  std::uint8_t* raw_start_ptr = reinterpret_cast<std::uint8_t *>(m_start_ptr);
 
-  // only do anything if ptr is the last elem
+  // only do anything if 'ptr' is the last element
   if (raw_start_ptr + m_offset == raw_mem_ptr + old_size) {
     std::size_t new_offset = m_offset + new_size - old_size;
     if (new_offset > m_total_size)
