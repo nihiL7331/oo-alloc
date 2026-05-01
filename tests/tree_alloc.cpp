8c97bac546d468bdd6771d95390a8daf2476507d
nihiL7331 <pujanekp6@gmail.com>
2026-05-01 18:01:58 +0200

    feat(tests): tree tests

---
+++ tests/tree_alloc.cpp
@@ -0,0 +1,22 @@
+#include "oo_alloc/FreeTreeAllocator.hpp"
+#include <cassert>
+#include <cstdint>
+
+int main() {
+  oo_alloc::FreeTreeAllocator tree;
+  tree.init(4096);
+
+  void* ptr1 = tree.alloc(128, 8);
+  assert(ptr1 != nullptr && "Failed to allocate 128B");
+  assert(reinterpret_cast<std::uintptr_t>(ptr1) % 8 == 0 && "Pointer is not 8B aligned");
+
+  void* ptr2 = tree.alloc(64, 1);
+  assert(ptr2 != nullptr && "Failed to allocate 64B");
+  assert(reinterpret_cast<std::uintptr_t>(ptr2) % alignof(void *) == 0 && "Failed to set min pointer alignment");
+
+  void* ptr_overflow = tree.alloc(tree.capacity() * 2, 8);
+  assert(ptr_overflow == nullptr && "Failed to return nullptr on overflow");
+
+  tree.free(ptr1);
+  tree.free(ptr2);
+}
---
+++ tests/tree_coal.cpp
@@ -0,0 +1,48 @@
+#include "oo_alloc/FreeTreeAllocator.hpp"
+#include <cassert>
+
+int main() {
+  oo_alloc::FreeTreeAllocator tree;
+  tree.init(16384);
+
+  void* p1 = tree.alloc(128, 8);
+  void* p2 = tree.alloc(128, 8);
+  void* p3 = tree.alloc(128, 8);
+  void* p4 = tree.alloc(128, 8);
+  void* barrier = tree.alloc(128, 8); 
+
+  assert(p1 && p2 && p3 && p4 && barrier && "Setup failed");
+
+  tree.free(p2);
+  
+  void* p2_reclaimed = tree.alloc(128, 8);
+  assert(p2_reclaimed == p2 && "Isolated free failed or didn't best-fit back into the same slot");
+  
+  tree.free(p3);
+  tree.free(p2_reclaimed); 
+
+  void* p23_merged = tree.alloc(256, 8);
+  assert(p23_merged == p2_reclaimed && "Merge right failed");
+
+  tree.free(p23_merged);
+  tree.free(p4);
+
+  void* p234_merged = tree.alloc(384, 8);
+  assert(p234_merged == p2_reclaimed && "Merge left failed");
+
+  tree.free(p234_merged);
+  tree.free(p1);
+  
+  p1 = tree.alloc(128, 8);
+  p2 = tree.alloc(128, 8);
+  p3 = tree.alloc(128, 8);
+  p4 = tree.alloc(128, 8);
+  
+  tree.free(p1);
+  tree.free(p3);
+  
+  tree.free(p2);
+  
+  void* merge_both_test = tree.alloc(384, 8);
+  assert(merge_both_test == p1 && "Merge both failed");
+}
