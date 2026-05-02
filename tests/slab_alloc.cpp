8ced939f1aca751872ba97999a855fc228cf8803
nihiL7331 <pujanekp6@gmail.com>
2026-05-02 21:39:24 +0200

    feat(tests, slab): alloc test

---
+++ tests/slab_alloc.cpp
@@ -0,0 +1,23 @@
+#include "oo_alloc/SlabAllocator.hpp"
+#include "oo_alloc/BuddyAllocator.hpp"
+#include "oo_alloc/utils.hpp"
+#include <cassert>
+
+int main() {
+  oo_alloc::BuddyAllocator buddy;
+  buddy.init(64 * 1024 * 1024);
+
+  oo_alloc::SlabAllocator slab(&buddy);
+  slab.init(oo_alloc::utils::page_size());
+
+  void* p1 = slab.alloc(16, 8);
+  assert(p1 != nullptr && "Failed standard small allocation");
+
+  void* p2 = slab.alloc(oo_alloc::utils::page_size() / 3, 8);
+  assert(p2 != nullptr && "Failed huge allocation routing");
+
+  for (int i = 0; i < 1000; ++i) {
+    void* p = slab.alloc(64, 8);
+    assert(p != nullptr && "Failed to allocate after multiple slab creations");
+  }
+}
