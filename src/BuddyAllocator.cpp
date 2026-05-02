bf7bfc5e8a886c5b0133f6396d846fb1a4b3dd47
nihiL7331 <pujanekp6@gmail.com>
2026-05-02 13:47:41 +0200

    feat(core, buddy): alloc logic

--- src/BuddyAllocator.cpp
+++ src/BuddyAllocator.cpp
@@ -2,6 +2,7 @@
 #include "oo_alloc/utils.hpp"
 #include <bit>
 #include <cassert>
+#include <cstdint>
 
 namespace oo_alloc {
 
@@ -11,9 +12,52 @@ void BuddyAllocator::split_block(std::size_t order) noexcept {
 }
 
 void* BuddyAllocator::alloc(std::size_t size, std::size_t align) {
-  (void)size; (void)align;
-  assert(false && "TODO");
-  return nullptr;
+  // calculate order
+  // this handles header + min sizes internally,
+  // no need for if statements for that here
+  std::size_t target_order = size_to_order(size);
+
+  // ensure that block is big enough to naturally align.
+  // since each blocks size is a power of 2,
+  // if MIN_BLOCK_SIZE << target_order >= align then the data is aligned
+  if ((MIN_BLOCK_SIZE << target_order) < align)
+    target_order = size_to_order(align - sizeof(AllocHeader));
+  
+  // if there's no free block of wanted order,
+  // split bigger blocks (of greater order)
+  // down to create it.
+  if (m_free_lists[target_order] == nullptr) {
+    std::size_t curr_order = target_order;
+
+    // find the smallest order, that is greater than 'target_order'
+    while (curr_order < MAX_ORDER && m_free_lists[curr_order] == nullptr)
+      curr_order++;
+
+    // if not found, return (not enough space)
+    if (curr_order == MAX_ORDER)
+      return nullptr;
+
+    //  split the block down
+    while (curr_order != target_order) {
+      split_block(curr_order);
+      curr_order--;
+    }
+  }
+
+  // pop the wanted block
+  FreeBlock* used_block = m_free_lists[target_order];
+
+  m_free_lists[target_order] = used_block->next;
+  if (m_free_lists[target_order] != nullptr)
+    m_free_lists[target_order]->prev = nullptr;
+
+  used_block->header.set_free(false);
+
+  // because of the 'FreeBlock' layout,
+  // user data begins where the 'prev' pointer
+  // sits when the block is free.
+  std::uintptr_t data_ptr = reinterpret_cast<std::uintptr_t>(used_block) + offsetof(FreeBlock, prev);
+  return reinterpret_cast<void *>(data_ptr);
 }
 
 void BuddyAllocator::free(void* ptr) {
