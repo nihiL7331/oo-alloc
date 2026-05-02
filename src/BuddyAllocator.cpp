b0da87f676d0e7be24b553323ef4575dd4829688
nihiL7331 <pujanekp6@gmail.com>
2026-05-02 13:11:16 +0200

    feat(core, buddy): init logic

--- src/BuddyAllocator.cpp
+++ src/BuddyAllocator.cpp
@@ -1,4 +1,6 @@
 #include "oo_alloc/BuddyAllocator.hpp"
+#include "oo_alloc/utils.hpp"
+#include <bit>
 #include <cassert>
 
 namespace oo_alloc {
@@ -20,9 +22,30 @@ void BuddyAllocator::free(void* ptr) {
 }
 
 bool BuddyAllocator::init(std::size_t size) {
-  (void)size;
-  assert(false && "TODO");
-  return false;
+  if (size < MIN_BLOCK_SIZE)
+    return false;
+
+  std::size_t target_size = std::bit_floor(size);
+
+  m_total_size = utils::align_up(target_size, utils::page_size());
+
+  m_start_ptr = utils::os_alloc(m_total_size);
+  if (!m_start_ptr)
+    return false;
+
+  m_free_lists.fill(nullptr);
+
+  std::size_t max_order = size_to_order(m_total_size);
+
+  FreeBlock* parent_block = static_cast<FreeBlock *>(m_start_ptr);
+  parent_block->header.set_order(max_order);
+  parent_block->header.set_free(true);
+
+  parent_block->prev = nullptr;
+  parent_block->next = nullptr;
+  m_free_lists[max_order] = parent_block;
+
+  return true;
 }
 
 void BuddyAllocator::clear() {
