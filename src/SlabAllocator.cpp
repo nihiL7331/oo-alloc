6fadcae73820e6c455e46c945234a05f38fb0de2
nihiL7331 <pujanekp6@gmail.com>
2026-05-02 18:50:59 +0200

    feat(core, slab): alloc logic

--- src/SlabAllocator.cpp
+++ src/SlabAllocator.cpp
@@ -12,9 +12,58 @@ SlabAllocator::~SlabAllocator() {
 }
 
 void* SlabAllocator::alloc(std::size_t size, std::size_t align) {
-  (void)size; (void)align;
-  assert(false && "TODO");
-  return nullptr;
+  // if the request is larger than half a page,
+  // it's too big for caches, so it needs to be
+  // allocated via the base allocator
+  if (size > m_page_size / 2)
+    return m_base_allocator->alloc(size, align);
+
+  // find the correct CacheManager
+  std::uint8_t cache_idx = size_to_cache_idx(size);
+  CacheManager& cache = m_caches[cache_idx];
+
+  // find a slab with free space, starting with
+  // the partial slabs
+  SlabHeader* active_slab = cache.partial_slabs;
+
+  if (active_slab == nullptr) {
+    // if there are no partial slabs,
+    // look in the empty slabs
+    active_slab = cache.empty_slabs;
+
+    if (active_slab != nullptr) {
+      // if there is a empty slab,
+      // move it to partial
+      remove_from_list(&(cache.empty_slabs), active_slab);
+      push_to_list(&(cache.partial_slabs), active_slab);
+    } else {
+      // if there are no empty slabs,
+      // initialize a new slab
+      active_slab = init_slab(cache_idx);
+      if (active_slab == nullptr)
+        return nullptr;
+
+      // after adding data it becomes a partial slab
+      push_to_list(&(cache.partial_slabs), active_slab);
+    }
+  }
+
+  // take the first free slot from the intrusive list
+  void* user_ptr = active_slab->free_list_head;
+
+  // read the pointer inside the free slot to find the next free slot
+  void** next_ptr = static_cast<void **>(user_ptr);
+  active_slab->free_list_head = *next_ptr;
+
+  active_slab->used++;
+
+  // if the slab just become full, move it to full slabs
+  if (active_slab->used == active_slab->capacity) {
+    remove_from_list(&(cache.partial_slabs), active_slab);
+    push_to_list(&(cache.full_slabs), active_slab);
+  }
+
+  return user_ptr;
 }
 
 void SlabAllocator::free(void* ptr) {
