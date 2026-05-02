a5e1d4f90bb456849ab57aaab921eb8cf3f3a75c
nihiL7331 <pujanekp6@gmail.com>
2026-05-02 14:58:18 +0200

    feat(tests, buddy): free test

---
+++ tests/buddy_free.cpp
@@ -0,0 +1,23 @@
+#include "oo_alloc/BuddyAllocator.hpp"
+#include <cassert>
+#include <vector>
+
+int main() {
+  oo_alloc::BuddyAllocator buddy;
+  bool succ = buddy.init(4 * 1024 * 1024);
+  assert(succ && "Failed to initialize");
+
+  std::vector<void*> ptrs;
+  for (int i = 0; i < 1024; ++i) {
+      void* ptr = buddy.alloc(1024, 8); 
+      assert(ptr != nullptr && "Failed to fragment heap");
+      ptrs.push_back(ptr);
+  }
+
+  for (auto it = ptrs.rbegin(); it != ptrs.rend(); ++it) {
+      buddy.free(*it);
+  }
+
+  void* giant_ptr = buddy.alloc((4 * 1024 * 1024) - 64, 8);
+  assert(giant_ptr != nullptr && "Failed to fully coalesce to max order");
+}
