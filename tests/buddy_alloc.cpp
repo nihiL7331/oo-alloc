3e4c4f180f7f4ef0caab6c6a24d064de7f07d932
nihiL7331 <pujanekp6@gmail.com>
2026-05-02 14:55:23 +0200

    feat(tests, buddy): alloc test

---
+++ tests/buddy_alloc.cpp
@@ -0,0 +1,25 @@
+#include "oo_alloc/BuddyAllocator.hpp"
+#include <cassert>
+
+int main() {
+  oo_alloc::BuddyAllocator buddy;
+  bool succ = buddy.init(1024 * 1024);
+  assert(succ && "Failed to initialize");
+
+  void* small_ptr = buddy.alloc(4, 8);
+  assert(small_ptr != nullptr && "Failed small allocation");
+
+  void* align_ptr = buddy.alloc(8, 64);
+  assert(align_ptr != nullptr && "Failed aligned allocation");
+  std::uintptr_t addr = reinterpret_cast<std::uintptr_t>(align_ptr);
+  assert((addr % 64 == 0) && "Failed to naturally align block");
+
+  int alloc_cnt = 0;
+  while (buddy.alloc(65536, 8) != nullptr) {
+      alloc_cnt++;
+  }
+  
+  assert(alloc_cnt > 0 && "Did not allocate any exhaustion blocks");
+  void* overflow_ptr = buddy.alloc(32, 8);
+  assert(overflow_ptr == nullptr && "Did not return nullptr when overflowed");
+}
