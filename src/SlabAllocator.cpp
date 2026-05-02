c8fa1e870e8ac5bd9bc9c9d88a63d9fcea1fdcc2
nihiL7331 <pujanekp6@gmail.com>
2026-05-02 17:56:33 +0200

    feat(core, slab): implementation structure

---
+++ src/SlabAllocator.cpp
@@ -0,0 +1,47 @@
+#include "oo_alloc/SlabAllocator.hpp"
+#include <cassert>
+
+namespace oo_alloc {
+
+SlabAllocator::SlabAllocator() {
+
+}
+
+SlabAllocator::~SlabAllocator() {
+
+}
+
+void* alloc(std::size_t size, std::size_t align) {
+  (void)size; (void)align;
+  assert(false && "TODO");
+  return nullptr;
+}
+
+void SlabAllocator::free(void* ptr) {
+  (void)ptr;
+  assert(false && "TODO");
+}
+
+bool SlabAllocator::init(std::size_t size) {
+  (void)size;
+  assert(false && "TODO");
+  return false;
+}
+
+void SlabAllocator::clear() {
+  assert(false && "TODO");
+}
+
+void* SlabAllocator::realloc(void* ptr, std::size_t old_size, std::size_t new_size, std::size_t align) {
+  (void)ptr; (void)old_size; (void)new_size; (void)align;
+  assert(false && "TODO");
+  return nullptr;
+}
+
+SlabAllocator::SlabHeader* SlabAllocator::init_slab(std::uint8_t cache_idx) noexcept {
+  (void)cache_idx;
+  assert(false && "TODO");
+  return nullptr;
+}
+
+}
