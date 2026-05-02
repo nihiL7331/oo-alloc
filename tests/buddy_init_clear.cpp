c052b89e7cd7ce79f798c14233884cc92965c0eb
nihiL7331 <pujanekp6@gmail.com>
2026-05-02 14:47:54 +0200

    feat(tests, buddy): init/clear test

---
+++ tests/buddy_init_clear.cpp
@@ -0,0 +1,20 @@
+#include "oo_alloc/BuddyAllocator.hpp"
+#include <cassert>
+
+int main() {
+    oo_alloc::BuddyAllocator buddy;
+
+    bool succ = buddy.init(100 * 1024 * 1024);
+    assert(succ && "Failed to initialize");
+
+    std::size_t actual_cap = buddy.capacity();
+    assert(actual_cap == (64 * 1024 * 1024) && "Failed to bit_floor to 64MB");
+
+    void* ptr1 = buddy.alloc(1024, 8);
+    assert(ptr1 != nullptr && "Failed basic allocation");
+
+    buddy.clear();
+
+    void* parent_ptr = buddy.alloc(actual_cap - 32, 8);
+    assert(parent_ptr != nullptr && "Clear failed to create the parent block");
+}
