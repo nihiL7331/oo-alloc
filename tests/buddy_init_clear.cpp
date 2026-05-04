8d9ab48d88e5ccafd404607575360550e9937261
nihiL7331 <pujanekp6@gmail.com>
2026-05-04 23:26:06 +0200

    fix(tests, buddy): wrong logic in buddy_init_clear

--- tests/buddy_init_clear.cpp
+++ tests/buddy_init_clear.cpp
@@ -5,7 +5,7 @@ int main() {
   oo_alloc::BuddyAllocator buddy(100 * 1024 * 1024);
 
   std::size_t actual_cap = buddy.capacity();
-  assert(actual_cap == (64 * 1024 * 1024) && "Failed to bit_floor to 64MB");
+  assert(actual_cap == (128 * 1024 * 1024) && "Failed to bit_ceil to 128MB");
 
   void* ptr1 = buddy.alloc(1024, 8);
   assert(ptr1 != nullptr && "Failed basic allocation");
