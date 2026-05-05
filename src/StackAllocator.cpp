d2bf549b6413dd59f41360125de037a29fd9cb36
nihiL7331 <pujanekp6@gmail.com>
2026-05-05 07:49:15 +0200

    feat(core, stack): owns method

--- src/StackAllocator.cpp
+++ src/StackAllocator.cpp
@@ -89,4 +89,11 @@ void StackAllocator::clear() {
   m_offset = 0;
 }
 
+bool StackAllocator::owns(void* ptr) const {
+  std::uintptr_t cast_ptr = reinterpret_cast<std::uintptr_t>(ptr);
+  std::uintptr_t start_ptr = reinterpret_cast<std::uintptr_t>(m_start_ptr);
+
+  return cast_ptr >= start_ptr && cast_ptr < start_ptr + m_total_size;
+}
+
 }
