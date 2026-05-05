ef2a6590c9ef6fc6200c21147ae86ba8d7ac499a
nihiL7331 <pujanekp6@gmail.com>
2026-05-05 07:49:03 +0200

    feat(core, pool): owns method

--- src/PoolAllocator.cpp
+++ src/PoolAllocator.cpp
@@ -107,5 +107,12 @@ void PoolAllocator::clear() {
 
   m_free_list_head = m_start_ptr;
 }
+
+bool PoolAllocator::owns(void* ptr) const {
+  std::uintptr_t cast_ptr = reinterpret_cast<std::uintptr_t>(ptr);
+  std::uintptr_t start_ptr = reinterpret_cast<std::uintptr_t>(m_start_ptr);
+
+  return cast_ptr >= start_ptr && cast_ptr < start_ptr + m_total_size;
+}
    
 }
