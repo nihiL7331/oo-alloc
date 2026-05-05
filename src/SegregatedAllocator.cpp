d7ab28d9b01d9ea00e81315858716d65ab89d2cc
nihiL7331 <pujanekp6@gmail.com>
2026-05-05 08:04:36 +0200

    feat(core, seg): owns logic

--- src/SegregatedAllocator.cpp
+++ src/SegregatedAllocator.cpp
@@ -1,5 +1,6 @@
 #include "oo_alloc/SegregatedAllocator.hpp"
 #include <cassert>
+#include <cstdint>
 
 namespace oo_alloc {
 
@@ -27,9 +28,10 @@ void SegregatedAllocator::clear() {
 }
 
 bool SegregatedAllocator::owns(void* ptr) const {
-  (void)ptr;
-  assert(false && "TODO");
-  return false;
+  std::uintptr_t cast_ptr = reinterpret_cast<std::uintptr_t>(ptr);
+  std::uintptr_t start_ptr = reinterpret_cast<std::uintptr_t>(m_start_ptr);
+
+  return cast_ptr >= start_ptr && cast_ptr < start_ptr + m_total_size;
 }
 
 }
